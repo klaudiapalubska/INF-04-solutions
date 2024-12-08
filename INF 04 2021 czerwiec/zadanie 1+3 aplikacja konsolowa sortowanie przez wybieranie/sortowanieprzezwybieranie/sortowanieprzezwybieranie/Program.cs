6a68db3de2687a63c9eff25145773db677fb1200
Klaudia <121185915+klaudiapalubska@users.noreply.github.com>
2024-12-08 20:54:08 +0100

    Add files via upload

---
+++ INF 04 2021 czerwiec/zadanie 1+3 aplikacja konsolowa sortowanie przez wybieranie/sortowanieprzezwybieranie/sortowanieprzezwybieranie/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace sortowanieprzezwybieranie
+{
+    using System;
+
+    public class Program
+    {
+
+
+        /********************************************************
+       * nazwa funkcji: ZnajdzMaksymalnaWartosc
+       * parametry wejściowe: tablica - tablica cyfr całkowitych
+       * wartość zwracana: zwracana zostaje maksymalna cyfra z tablicy jako int
+       * autor: xyz
+       * ****************************************************/
+        private static int ZnajdzMaksymalnaWartosc(int[] tablica)
+        {
+            int maksymalna = tablica[0]; 
+            for (int i = 1; i < tablica.Length; i++)
+            {
+                if (tablica[i] > maksymalna)
+                {
+                    maksymalna = tablica[i];
+                }
+            }
+            return maksymalna;
+        }
+
+        /********************************************************
+        * nazwa funkcji: SortujTablice
+        * parametry wejściowe: tablica - tablica cyfr całkowitych
+        * wartość zwracana: brak
+        * autor: xyz
+        * ****************************************************/
+        public static void SortujTablice(int[] tablica)
+        {
+            for (int i = 0; i < tablica.Length - 1; i++)
+            {
+                int indeksMin = i;
+                for (int j = i + 1; j < tablica.Length; j++)
+                {
+                    if (tablica[j] < tablica[indeksMin])
+                    {
+                        indeksMin = j;
+                    }
+                }
+                // Zamiana miejscami
+                int temp = tablica[i];
+                tablica[i] = tablica[indeksMin];
+                tablica[indeksMin] = temp;
+            }
+        }
+
+       
+        public static void WyswietlTablice(int[] tablica)
+        {
+            foreach (var el in tablica)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] tablica = new int[10];
+            Console.WriteLine("Wypisz dziesięć cyfr do posortowania");
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                string text = Console.ReadLine();
+
+                tablica[i] = Convert.ToInt32(text);
+            }
+         
+            SortujTablice(tablica);
+            Console.WriteLine("Tablica po posortowaniu:");
+            WyswietlTablice(tablica);
+
+            
+            int maksymalna = ZnajdzMaksymalnaWartosc(tablica);
+            Console.WriteLine("Maksymalna wartość: " + maksymalna);
+        }
+    }
+
+
+
+
+}
---
+++ INF 04 2021 czerwiec/zadanie 2 aplikacja mobilna/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace rejestracjakonta
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string emailEntry = email.Text;
+            if (haslo.Text != haslo2.Text || haslo.Text == null)
+            {
+                 komunikat.Text = "Hasła się różnią"; 
+            }else if (emailEntry.Contains("@") && haslo.Text == haslo2.Text)
+            {
+              
+                    komunikat.Text = "Witaj "+emailEntry;
+            }
+            else
+            {
+                komunikat.Text = "Nieprawidłowy adres email";
+            }
+        }
+    }
+}
